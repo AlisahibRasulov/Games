d1c96dbc4bae151fb3e0adc14f1bfcf8d6a6d554
Alisahib Rasulov <alisahibrasulov@gmail.com>
2026-02-10 00:02:41 +0400

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { useState } from 'react';
 import './App.scss'
-import Game1 from './components/Game1';
+// import Game1 from './components/Game1';
 import Game2 from './components/Game2';
 
 function App() {
--- src/components/Game2.jsx
+++ src/components/Game2.jsx
@@ -1,73 +1,4 @@
-// import React, { useState } from "react";
-
-// const Game2 = () => {
-//   const choices = ["dash", "kagiz", "qayci"];
-//   const [player, setPlayer] = useState(null);
-//   const [computer, setComputer] = useState(null);
-//   const [result, setResult] = useState("");
-
-//   function getComputerChoice() {
-//     const random = Math.floor(Math.random() * choices.length);
-//     return choices[random];
-//   }
-
-//   function playGame(playerChoice) {
-//     const computerChoice = getComputerChoice();
-//     setPlayer(playerChoice);
-//     setComputer(computerChoice);
-
-//     const win =
-//       (playerChoice == "kagiz" && computerChoice == "dash") ||
-//       (playerChoice == "qayci" && computerChoice == "kagiz") ||
-//       (playerChoice == "dash" && computerChoice == "qayci");
-
-//     if (playerChoice == computerChoice) {
-//       setResult("oyun beraberdir");
-//     } else if (win) {
-//       setResult("uddun");
-//     } else {
-//       setResult("uduzdun");
-//     }
-//   }
-//   function resetGame() {
-//     setPlayer(null);
-//     setComputer(null);
-//     setResult("");
-//   }
-//   return (
-//     <>
-//       {result ? (
-//         <div className="container">
-//           <div className="cart">
-//             <div className="result">
-//               <p>Sen: {player}</p>
-//               <p>Komputer: {computer}</p>
-//               <h1>{result}</h1>
-//             </div>
-//             <div className="buttons">
-//               <button onClick={resetGame}>Yeniden bashla</button>
-//             </div>
-//           </div>
-//         </div>
-//       ) : (
-//         <div className="container">
-//           <div className="card">
-//             <div className="buttons">
-//               <button onClick={() => playGame(choices[0])}>Das</button>
-//               <button onClick={() => playGame(choices[1])}>Kagiz</button>
-//               <button onClick={() => playGame(choices[2])}>Qayci</button>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Game2;
-
 import React, { useState } from "react";
-
 
 const Game2 = () => {
   const choices = ["dash", "kagiz", "qayci"];
@@ -95,11 +26,11 @@ const Game2 = () => {
       (playerChoice === "dash" && computerChoice === "qayci");
 
     if (playerChoice === computerChoice) {
-      setResult("oyun beraberdir");
+      setResult("Draw 🤝");
     } else if (win) {
-      setResult("uddun");
+      setResult("Winner 🏆");
     } else {
-      setResult("uduzdun");
+      setResult("You lost 😞");
     }
   }
 
